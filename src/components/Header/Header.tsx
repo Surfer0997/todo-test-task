3682712d3bfd4a853faf0b437e5bfd8ebe879cdb
Surfer0997 <artanagibaet228@gmail.com>
2022-12-10 21:15:22 +0200

    Added post-header message, changed header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 export const Header = () => {
   return (
     <div>
-      <h2 style={{ margin: '0 0 1rem 0', padding: '1rem' }}>Fancy ToDo app</h2>
+      <h2 style={{ padding: '1rem 0 0 0' }}>Fancy ToDo app</h2>
+      <p style={{ padding: '1rem', color:"aliceblue"}}>Made only for you!</p>
     </div>
   );
 };
