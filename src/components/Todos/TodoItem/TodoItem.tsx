4bf3b8b89b34080b863c526e117290e65c6c3e76
Surfer0997 <artanagibaet228@gmail.com>
2022-12-11 12:18:07 +0200

    Made animation for Todo Items

--- src/components/Todos/TodoItem/TodoItem.tsx
+++ src/components/Todos/TodoItem/TodoItem.tsx
@@ -2,38 +2,33 @@ import styled from 'styled-components';
 import { Todo } from '../../../types/todoTypes';
 import { TodoControls } from './TodoControls';
 import { TodoUpdateForm } from './TodoUpdateForm';
+import './TodoItem.css';
 
 interface TodoItemProps {
   todo: Todo;
 }
 
-
-
 export const TodoItem = ({ todo }: TodoItemProps) => {
   const { id, text, done, editing } = todo;
 
-
   const StyledTodoContainer = styled.div`
-  text-decoration: ${done ? 'line-through' : ''};
-  width: 90%;
-  margin-left: 5%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding-bottom: .5rem;
-  border-bottom: solid;
-  margin-bottom: .5rem;
+    text-decoration: ${done ? 'line-through' : ''};
+    width: 90%;
+    margin-left: 5%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-bottom: 0.5rem;
+    border-bottom: solid;
+    margin-bottom: 0.5rem;
   `;
-  
 
   return (
-    <StyledTodoContainer>
-      {!editing ? (
-        <span>{text}</span>
-      ) : (
-        <TodoUpdateForm id={id} text={text}/>
-      )}
-      {!editing && <TodoControls todo={todo}/>}
-    </StyledTodoContainer>
+    <div>
+      <StyledTodoContainer>
+        {!editing ? <span>{text}</span> : <TodoUpdateForm id={id} text={text} />}
+        {!editing && <TodoControls todo={todo} />}
+      </StyledTodoContainer>
+    </div>
   );
 };
--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -4,32 +4,41 @@ import { RootState } from '../../store/store';
 import { Card } from '../UI/Card';
 import { NewTodoForm } from './NewTodoForm/NewTodoForm';
 import { TodoItem } from './TodoItem/TodoItem';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Todo } from '../../types/todoTypes';
 
 const TodoHeader = styled.div`
   text-align: left;
-  background:#61dafb;
+  background: #61dafb;
   color: white;
   width: 100%;
-  padding: .5rem 0;
+  padding: 0.5rem 0;
   text-indent: 1rem;
   border-top-right-radius: 3px;
   border-top-left-radius: 3px;
 `;
 
+const displayTodos = (todos:Todo[]) => {
+    return todos.map(item => (
+      <CSSTransition timeout={500} classNames="todo-item todo-item" key={item.id}>
+        <TodoItem todo={item} />
+      </CSSTransition>
+    ))
+}
 
 export const Todos = () => {
   const todos = useSelector((state: RootState) => state.todo.todos);
 
   return (
     <Card>
-      <TodoHeader className="todo-header"><h3>ToDo app</h3></TodoHeader>
+      <TodoHeader className="todo-header">
+        <h3>ToDo app</h3>
+      </TodoHeader>
       <div className="todo-body">
         <NewTodoForm />
-        <div className="todos">
-          {todos.map(item => (
-            <TodoItem todo={item} key={item.id} />
-          ))}
-        </div>
+        <TransitionGroup component="div"  className="list">
+          {displayTodos(todos)}
+        </TransitionGroup>
       </div>
     </Card>
   );
